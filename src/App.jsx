6d77a2d292609f6f60ebc02536697db10c3aee9c
Chillfar <chillfar@hotmail.com>
2025-03-25 12:12:59 +0000

    Agrega logo en formato JPG y SVG, y actualiza el diseño del GameDashboard

--- src/App.jsx
+++ src/App.jsx
@@ -75,11 +75,12 @@ export default function GameDashboard() {
 
   return (
     <Container maxWidth={false} style={{ padding: "16px", backgroundColor: "#121212", color: "white", minHeight: "100vh", width: "100vw" }}>
-      <Typography variant="h4" align="center" gutterBottom>
-        XGN Lestedo '25
-      </Typography>
+      <div style={{ textAlign: "left", marginLeft:"25px", marginBottom: "20px" }}>
+        <img src="/logo.jpg" alt="Logo" style={{ maxWidth: "200px" }} />
+      </div>
       
-      <Rnd default={{ x: 20, y: 100, width: "40%", height: "auto" }}>
+      {/* Panel de Ranking */}
+      <Rnd default={{ x: 40, y: 140, width: "40%", height: "auto" }}>
         <Paper style={{ padding: "16px", backgroundColor: "#1e1e1e", color: "white" }}>
           <Typography variant="h5" gutterBottom>Ranking</Typography>
           <Table>
@@ -108,7 +109,7 @@ export default function GameDashboard() {
       </Rnd>
 
       {/* Panel de Juegos */}
-      <Rnd default={{ x: 1400, y: 100, width: "19%", height: "auto" }}>
+      <Rnd default={{ x: 1510, y: 140, width: "19%", height: "auto" }}>
         <Paper style={{ padding: "16px", backgroundColor: "#1e1e1e", color: "white" }}>
           <Typography variant="h5" gutterBottom>Selecciona un juego para puntuar</Typography>
           <Grid container spacing={2}>
@@ -127,7 +128,7 @@ export default function GameDashboard() {
       </Rnd>
         
       {/* Panel de Evolución */}
-      <Rnd default={{ x: 820, y: 100, width: "24%", height: "auto" }}>
+      <Rnd default={{ x: 820, y: 140, width: "24%", height: "auto" }}>
         <Paper style={{ padding: "16px", backgroundColor: "#1e1e1e", color: "white" }}>
             <Typography variant="h5" gutterBottom>Evolución de Puntos</Typography>
             <Line data={chartData} />
@@ -136,16 +137,16 @@ export default function GameDashboard() {
       </Rnd>
 
       {/* Panel de Vídeos */}
-      <Rnd default={{ x: 20, y: 460, width: "40%", height: "auto" }}>
-        <Paper style={{ padding: "16px", textAlign: "center", backgroundColor: "#1e1e1e", color: "white" }}>
+      <Rnd default={{ x: 40, y: 500, width: "40%", height: "auto" }}>
+        <Paper style={{ padding: "16px", backgroundColor: "#1e1e1e", color: "white" }}>
           <Typography variant="h5" gutterBottom>Party Vídeos</Typography>
           <iframe width="100%" height="300" src="https://www.youtube.com/embed/videoseries?si=yt9cPirnyjTM-f3_&amp;list=PL2ihC4aJWkWpD8C2MJ62Cx80abK9l-I4L" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
         </Paper>
       </Rnd>
 
       {/* Panel del Mapa */}
-      <Rnd default={{ x: 820, y: 460, width: "19%", height: "auto" }}>
-        <Paper style={{ padding: "16px", textAlign: "center", backgroundColor: "#1e1e1e", color: "white" }}>
+      <Rnd default={{ x: 820, y: 500, width: "24%", height: "auto" }}>
+        <Paper style={{ padding: "16px", backgroundColor: "#1e1e1e", color: "white" }}>
           <Typography variant="h5" gutterBottom>Ubicación de LAN party</Typography>
           <iframe
             title="Ubicación"
