7b53d0755c5368d489b33570ca0df154b0f42a27
Chillfar <chillfar@hotmail.com>
2025-03-27 09:53:24 +0000

    Elimina pruebas de App.test.jsx ya que la aplicación ha sido renombrada a GameDashboard.

--- src/App.test.jsx
+++
@@ -1,9 +0,0 @@
-import { expect, test } from 'vitest';
-import { render, screen } from '@testing-library/react';
-import GameDashboard from './App';
-
-test('renders learn react link', () => {
-  render(<GameDashboard />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeDefined();
-});
