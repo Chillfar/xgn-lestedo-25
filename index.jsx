824bb7b08f21b389d4f65b88e0a2c977ae9310a8
Chillfar <chillfar@hotmail.com>
2025-04-06 22:33:18 +0200

    Refactors import paths for CSS and App component in index.jsx; remove unused index.css file

--- index.jsx
+++ index.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import GameDashboard from './App';
+import './src/index.css';
+import GameDashboard from './src/App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
