d117a5accc428b3a14bdf21ce2c52d831546e8ff
Chillfar <chillfar@hotmail.com>
2026-03-08 00:54:37 +0100

    Create GamePanel component

---
+++ src/components/GamePanel.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const GamePanel = () => {
+    return (
+        <div>
+            <h1>Game Panel</h1>
+            {/* Add your game panel functionality here */}
+        </div>
+    );
+};
+
+export default GamePanel;
