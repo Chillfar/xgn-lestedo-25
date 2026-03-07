243fdb00eced37a015750a778894d3f1e0a4bca7
Chillfar <chillfar@hotmail.com>
2026-03-08 00:54:50 +0100

    Create RankingPanel component

---
+++ src/components/RankingPanel.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const RankingPanel = () => {
+    return (
+        <div>
+            <h2>Ranking Panel</h2>
+        </div>
+    );
+};
+
+export default RankingPanel;
